c728aac7ea8d2b5af1901abfa976f946fcdecf64
Anthony Vigliotta <anthonyvigz@gmail.com>
2019-10-21 20:44:53 -0500

    adding nav bar

--- eventr-app/src/App.js
+++ eventr-app/src/App.js
@@ -4,7 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     
+      <div className="mainNav">
+        <h1 className="eventrLogo">eventr</h1>
+        <nav>
+          <a href="#">Account</a>
+          <a href="#">About</a>
+          <a href="#">Contact</a>
+        </nav>
+      </div>
     </div>
   );
 }
