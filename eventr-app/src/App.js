86277a31e1f1f8b852fe68335d6f7ae7b9786bb9
Anthony Vigliotta <anthonyvigz@gmail.com>
2019-10-28 13:07:43 -0500

    smoothed transition between forms

--- eventr-app/src/App.js
+++ eventr-app/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
-import { Route, Switch, withRouter } from 'react-router-dom';
 import HomeForm from './components/HomeForm';
+import { useSpring, animated } from 'react-spring';
 
 function App() {
+  const props = useSpring({ opacity: 1, from: { opacity: 0 }})
+  
   return (
     <div className="App">
       <div className="mainNav">
-        <h1 className="eventrLogo">eventr</h1>
+        <animated.div style={props}>
+          <h1 className="eventrLogo">eventr</h1>
+        </animated.div>
         <nav>
           <a href="#">Account</a>
           <a href="#">About</a>
