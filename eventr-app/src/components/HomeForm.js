97a09be5fac5ddf6c3cd0c24ba27c933ff160822
Anthony Vigliotta <anthonyvigz@gmail.com>
2019-11-11 14:48:44 -0600

    refactored step0 to use hooks

--- eventr-app/src/components/HomeForm.js
+++ eventr-app/src/components/HomeForm.js
@@ -11,9 +11,9 @@ function HomeForm() {
 
     const { location } = useContext(__RouterContext);
     const transitions = useTransition(location, location => location.pathname, {
-        from: { opacity: 0 },
-        enter: { opacity: 1 },
-        leave: { opacity: 0 }
+        from: { opacity: 0, transform: 'translate3d(10%,0,0)' },
+        enter: { opacity: 1, transform: 'translate3d(0%,0,0)'},
+        leave: { opacity: 0, transform: 'translate3d(-10%,0,0)' }
         });
 
         return (
--- eventr-app/src/components/eventStepsComponents/Step0.jsx
+++ eventr-app/src/components/eventStepsComponents/Step0.jsx
@@ -4,55 +4,62 @@ import { connect } from 'react-redux';
 import { addEventType } from '../../actions/index';
 import AbsoluteWrapper from '../AbsoluteWrapper';
 import { useSpring, useTransition, animated } from 'react-spring';
+import { Spring } from 'react-spring/renderprops'
 
 
-class Step0 extends Component {
-    constructor() {
-        super();
-        this.state = {
-        }
-    }
+function Step0(props) {
 
-    makePrivateEvent = event => {
+    const makePrivateEvent = event => {
         event.preventDefault();
 
-        this.props.addEventType('private');
+        props.addEventType('private');
 
-        this.props.history.push('/step1')
+        props.history.push('/step1')
     }
 
-    makePublicEvent = event => {
+    const makePublicEvent = event => {
         event.preventDefault();
 
-        this.props.addEventType('public');
+        props.addEventType('public');
 
-        this.props.history.push('/step1')
+        props.history.push('/step1')
     }
-
-
-    render() {
+    
+    const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1.1]
+    const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
         
+
         return (
             <AbsoluteWrapper>
                 <div className="stepsBlock">
                     <div className="eventStarter">
                         <div className="textwrapper">
-                            <h2>What type of event is this, {this.props.firstname}?</h2>
+                            <h2>What type of event is this, {props.firstname}?</h2>
                         </div>
                         <div className="selectEvent">
-                            <button onClick={this.makePrivateEvent} className="eventType">
-                                Private
-                            </button>
-                            <button onClick={this.makePublicEvent} className="eventType">
-                                Public
-                            </button>
+                            <Spring
+                                from={{ opacity: 0 }}
+                                to={{ opacity: 1 }}
+                                delay='1000'>
+                                {props => <div style={props}><button onClick={makePrivateEvent} className="eventType">
+                                    Private
+                                </button></div>}
+                            </Spring>
+                            <Spring
+                                from={{ opacity: 0 }}
+                                to={{ opacity: 1 }}
+                                delay='1000'>
+                                {props => <div style={props}><button onClick={makePublicEvent} className="eventType">
+                                    Public
+                                </button></div>}
+                            </Spring>
                         </div>
                     </div>
                 </div>
             </AbsoluteWrapper>
         );
     }
-}
+
 
 const mapStateToProps = (state) => {
     return {
