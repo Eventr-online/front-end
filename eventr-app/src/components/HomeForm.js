b5f74f9ff3fa8d74233a3f3f155b9459c0d1c45f
Anthony Vigliotta <anthonyvigz@gmail.com>
2019-11-11 10:14:55 -0600

    centering transitions

--- eventr-app/src/components/HomeForm.js
+++ eventr-app/src/components/HomeForm.js
@@ -1,12 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component, useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 import Register from './Register';
 import Login from './Login';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import { Route, Switch, withRouter } from 'react-router-dom';
 import '../css/homeform.css';
+import { Route, Switch, withRouter, __RouterContext } from 'react-router-dom';
+import { useTransition, animated } from 'react-spring';
+
 
 function HomeForm() {
+
+    const { location } = useContext(__RouterContext);
+    const transitions = useTransition(location, location => location.pathname, {
+        from: { opacity: 0 },
+        enter: { opacity: 1 },
+        leave: { opacity: 0 }
+        });
 
         return (
         <div className="formBackground">
@@ -14,20 +22,14 @@ function HomeForm() {
                 <NavLink activeClassName="activeLink" exact to="/">Register</NavLink>
                 <NavLink activeClassName="activeLink" exact to="/login">Login</NavLink>
             </div>
-            <Route render={({location}) => (
-                <TransitionGroup>
-                <CSSTransition
-                    key={location.key}
-                    timeout={3000}
-                    classNames="fade"
-                >
-                    <Switch location={location}>
+            {transitions.map(({ item, props, key }) => (
+            <animated.div key={key} style={props}>
+                <Switch location={item}>
                     <Route exact path="/" component={Register} />
                     <Route exact path="/login" component={Login} />
-                    </Switch>  
-                </CSSTransition>   
-                </TransitionGroup>   
-            )} />
+                </Switch>  
+            </animated.div>
+        ))}
         </div>
         );
   }
