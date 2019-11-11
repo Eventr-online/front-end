5dd382c3a133a1748a223585344d53d8419c9d6d
Anthony Vigliotta <anthonyvigz@gmail.com>
2019-11-10 18:49:16 -0600

    setting up switch for event creator

--- eventr-app/src/components/EventCreation.js
+++ eventr-app/src/components/EventCreation.js
@@ -1,39 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component, useContext } from 'react';
 import '../css/homeform.css';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter, __RouterContext } from 'react-router-dom';
 import { CSSTransition, TransitionGroup } from 'react-transition-group'; 
 import Step1 from '../components/eventStepsComponents/Step1';
 import Step2 from '../components/eventStepsComponents/Step2';
+import { useTransition, animated } from 'react-spring';
 
-class EventCreation extends Component {
-    constructor() {
-        super();
-        this.state = {
+function EventCreation() {
 
-        }
-    }
+    const { location } = useContext(__RouterContext);
+    const transitions = useTransition(location, location => location.pathname, {
+        from: { opacity: 0, transform: "translate(100%, 0)" },
+        enter: { opacity: 1, transform: "translate(0, 100%)" },
+        leave: { opacity: 0, transform: "translate(-50%, 0)" },
+    })
 
-    render() {
-        return (
-            <div>
-                <h1> HEY </h1>
-                <Route render={({location}) => (
-                    <TransitionGroup>
-                    <CSSTransition
-                        key={location.key}
-                        timeout={3000}
-                        classNames="fade"
-                    >
-                        <Switch location={location}>
+    return (
+        <div className="eventcreatebox">
+            {transitions.map(({ item, props, key }) => (
+                <animated.div key={key} style={props}>
+                    <Switch location={item}>
                         <Route exact path="/eventcreate" component={Step1} />
                         <Route exact path="/step2" component={Step2} />
-                        </Switch>  
-                    </CSSTransition>   
-                    </TransitionGroup>   
-                )} />
-            </div>
-        );
-    }
+                    </Switch>
+                </animated.div>         
+            ))}                    
+        </div>
+    );
 }
 
 export default EventCreation;
